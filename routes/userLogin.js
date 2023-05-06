a1ddf5b2346d48b236b65a1a46949594b46bdf32
Yoseph <ymymym4634@gmail.com>
2023-05-06 19:39:38 +0300

    Updates

--- routes/userLogin.js
+++ routes/userLogin.js
@@ -2,10 +2,18 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const { ACCESS_TOKEN } = require("../config");
+const authenticate = require("../authenticate/auth");
 
 // login route
-router.post('/api/login', async (req, res) => {
+router.post('/api/login', authenticate ,async (req, res) => {
     const { email, password } = req.body;
+
+    // authenticate the user
+    const accessToken = jwt.sign({ email }, ACCESS_TOKEN);
+    res.json({ accessToken: accessToken });
+
 
     // Check if the user exists in the database
     const user = await User.findOne({ email });
@@ -23,6 +31,7 @@ router.post('/api/login', async (req, res) => {
     }
 
     // Return a success message if the user is found and the password matches
+    res.setHeader('X-Foo', 'bar')
     return res.send(`Welcome Back, ${user.name} :)`).json({ message: "Login successful" });
     
 });
