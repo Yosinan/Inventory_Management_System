62d40e20ef7845f92ac9bafa47e240bea260b69c
Yoseph <ymymym4634@gmail.com>
2023-05-13 20:10:28 +0300

    updates

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 require('express-async-errors');
 const express = require("express");
-const cookieparser = require("cookie-parser");
+// const cookieparser = require("cookie-parser");
 const start = require('./database');
 const userRouter = require("./routes/userRoute");
 const productRouter = require("./routes/productRoute");
-const userLogin = require("./routes/userLogin");
 const statusRouter = require("./routes/status");
 const errorHandler = require('./middlewares/errorHandler');
 const cors = require("cors");
@@ -20,7 +19,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Routes
-// app.use('/', userLogin);
 app.use('/', userRouter);
 app.use('/', productRouter);
 app.use('/', statusRouter);
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middlewares/auth");
-const {  getUser, getUserById, registerUser, loginUser } = require("../controllers/userController");
+const {  getUser, getUserById, registerUser, loginUser, logoutUser } = require("../controllers/userController");
 
 // using the APIs
 router.post("/api/users/register", registerUser);
 router.post("/api/users/login", loginUser);
 router.get("/api/users/profile", auth , getUser);
 router.get("/api/users/:id", getUserById );
+router.get("/api/users/logout", logoutUser);
 
 module.exports = router;
